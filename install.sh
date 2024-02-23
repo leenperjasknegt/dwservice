7b2c5a2f37bbc7b0600a262ed8571e27ca237d9a
leenperjasknegt <leenknegtjasper@gmail.com>
2024-02-23 18:07:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,22 +7,20 @@
 
 sudo apt update
 echo
-#if [ $(dpkg-query -W -f='${Status}' wget 2>/dev/null | grep -c "ok installed") -eq 0 ];
-#then
+if [ $(dpkg-query -W -f='${Status}' wget 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
   echo
   echo "###################################################################"
   echo "Installing Wget"
   echo "###################################################################"
   echo
-#  sudo apt install wget;
-#fi
-then
+  sudo apt install wget;
+fi
   echo
   echo "###################################################################"
   echo ""
   echo "###################################################################"
   echo
-fi
 wget https://www.dwservice.net/download/dwagent.sh
 sudo chmod +x dwagent.sh
 sudo ./dwagent.sh
